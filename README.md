1434e1fa16f05d1a155136f05e32369f5935dc6d
Allan Nava <allannava95@gmail.com>
2020-09-17 08:21:19 +0200

    update const for index.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # github-activity-file
+
+Updates FILE with the recent GitHub activity of a user.
+
+Inspired by https://github.com/jamesgeorge007/github-activity-readme
--- index.js
+++ index.js
@@ -7,3 +7,16 @@
  *  Updated by [ Allan Nava ]
  *  Copyright © 2020 [ Allan Nava ]. All rights reserved.
  */
+const core          = require("@actions/core");
+const fs            = require("fs");
+const path          = require("path");
+const { spawn }     = require("child_process");
+const { Toolkit }   = require("actions-toolkit");
+//
+const MAX_LINES = 5;
+//
+// Get config
+const GH_USERNAME   = core.getInput("GH_USERNAME");
+const COMMIT_MSG    = core.getInput("COMMIT_MSG");
+const FILE          = core.getInput("FILE");
+//
