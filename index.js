80f8a9247a57409741ac8111adeb6b569a3c15a7
Allan Nava <allannava95@gmail.com>
2020-09-17 08:17:35 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ *  index.js
+ *  Allan Nava 2020-09-17
+ *  Allan Nava 2020-09-17
+ *
+ *  Created by [ Allan Nava ].
+ *  Updated by [ Allan Nava ]
+ *  Copyright © 2020 [ Allan Nava ]. All rights reserved.
+ */
